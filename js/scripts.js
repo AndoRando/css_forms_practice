f62e01346292824cf5ae10cb54e51e620a6ac109
Andrew <student@epicodus.com>
2015-12-01 16:24:11 -0800

    Implement form content.

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$('.form').find('input, textarea').on('keyup blur focus', function(e) {
+  var $this = $(this);
+  var label = $this.prev('label');
+
+  if (e.type === 'keyup') {
+    if ($this.val() === '') {
+      label.removeClass('active');
+    } else {
+      label.addClass('active');
+    }
+  }
+});
+
+$('.tab a').on('click', function(e) {
+
+  e.preventDefault();
+
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+
+  $(target).fadeIn(600);
+
+});
